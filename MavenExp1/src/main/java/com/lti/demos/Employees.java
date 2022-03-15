f9927edaaa11165861b223be8c4e7c577a656983
Shanmugapriya111 <shanmugapriyashanmugam99@gmail.com>
2022-03-16 00:06:16 +0530

    first commit

---
+++ MavenExp1/src/main/java/com/lti/demos/Employees.java
@@ -0,0 +1,13 @@
+package com.lti.demos;
+
+public class Employees {
+
+	public static void main(String[] args) {
+		EmpTax e = new EmpTax();
+		double t=e.tax(26000);
+		System.out.println("Employee Tax : " + t);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MavenExp1/src/test/java/com/lti/demos/EmployeeTest.java
@@ -0,0 +1,36 @@
+package com.lti.demos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	public double tax(double salary) {
+		
+		double Emptax = (salary/12)+(salary*0.12);
+		return Emptax;
+		
+	}
+
+	@Test
+	void test() {
+		EmpTax e = new EmpTax();
+		double result = e.tax(26000);
+		
+		Assertions.assertEquals(5286.67, Math.round(result*100.0)/100.);
+		Assertions.assertNotNull(result);
+		
+		double result1 =  e.tax(0.2);
+		boolean flag=false;
+		if(result1==0.00) {
+			flag=true;
+		}
+		Assertions.assertFalse(flag);
+		
+		
+	}
+	}
+
+
