e956809df901f2383b1cdf3c4887b2cb77c3450d
Shanmugapriya111 <shanmugapriyashanmugam99@gmail.com>
2022-03-16 17:41:20 +0530

    first commit

---
+++ MavenExp/src/test/java/git/com/EmployeTest.java
@@ -0,0 +1,41 @@
+package git.com;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class EmployeTest {
+
+
+		public double tax(double salary) {
+			
+			double Emptax = (salary/12)+(salary*0.12);
+			return Emptax;
+			
+		}
+
+		@Test
+		void test() {
+			EmpTax e = new EmpTax();
+			double result = e.tax(26000);
+			
+			Assertions.assertEquals(5286.67, Math.round(result*100.0)/100.);
+			Assertions.assertNotNull(result);
+			
+			double result1 =  e.tax(0.2);
+			boolean flag=false;
+			if(result1==0.00) {
+				flag=true;
+			}
+			Assertions.assertFalse(flag);
+			
+			
+		}
+		}
+
+
+	
+
+
